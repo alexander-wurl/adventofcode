671a280b1ea0853a8ade2d155ba75479d5992935
Alexander Wurl <alexander.wurl@gmx.de>
2020-12-15 02:51:49 +0100

    7 with aoc, update for 6

---
+++ adventofcode-07.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from aoc import read_input
+
+def substitute(shinygoldrules, rules):
+  for r in rules:  
+    for g in shinygoldrules:  
+      if (r[1].find(g[0][:-1])>-1):  
+        temp = [str(r[0]), str(r[1].replace(g[0][:-1], g[1]))] 
+        shinygoldrules.append(temp)
+  return shinygoldrules
+
+def finduniqe(rules):
+  leftside = []
+  leftside = [e[0] for e in rules]
+  ret = []
+  for e in leftside:
+    if (e not in ret):
+      ret.append(e)
+  return ret
+
+# main
+data = read_input(2020, 7)
+
+rules = []
+rules = data.split("\n")[:-1] 
+
+splittedrules = []
+splittedrules = [x.split(" contain ") for x in rules] 
+
+shinygoldrules = []
+for e in splittedrules: 
+  if (e[1].find("shiny gold bag")>-1): 
+    shinygoldrules.append(e)
+
+solution = 0
+
+while True:
+  shinygoldrules = substitute(shinygoldrules, splittedrules)
+  l = len(finduniqe(shinygoldrules))
+  if ( l > solution):
+      solution = l
+  else:
+      break
+
+print( solution )
