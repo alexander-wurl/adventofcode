3ba6b5c4c269bde0604cee0d6e24b99e280ec7c9
alexander-wurl <alexander.wurl@gmx.de>
2020-12-25 20:16:44 +0100

    minor changes

--- adventofcode-09.py
+++ adventofcode-09.py
@@ -4,7 +4,6 @@ import helper
 def isValidNumber(preamble, number):
     for e in combinations(preamble, 2):
 
-        #print(str(e[0]) + " + " + str(e[1]) + " = " + str(number))
         sum = int(e[0]) + int(e[1])
 
         if (e[0] != e[1]) and (sum == int(number)):
