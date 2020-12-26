069b2f52472a33447926be87f57888d148f1ed79
alexander-wurl <alexander.wurl@gmx.de>
2020-12-26 01:24:18 +0100

    stop condition for recursion, final 'data' element

--- adventofcode-10.py
+++ adventofcode-10.py
@@ -1,7 +1,7 @@
 import helper
 
-def recTest(voltage, d1, d2, d3):
-    # find elements in list which are 1, 2, or 3 lower 
+def recursion(voltage, d1, d2, d3):
+    # find elements in list which are 1, 2, or 3 lower
     # then use each of them for recursive call
 
     global diff1
@@ -14,38 +14,39 @@ def recTest(voltage, d1, d2, d3):
             if (e - voltage == 1):
                 # diff = 1
                 diff1.append("1|" + str(e) + "|" + str(voltage))
-                print("1|" + str(e) + "|" + str(voltage))
-                recTest(e, d1+1, d2, d3)
+                recursion(e, d1 + 1, d2, d3)
+                data.remove(e)
             elif (e - voltage == 2):
                 # diff = 2
                 diff2.append("2|" + str(e) + "|" + str(voltage))
-                print("2|" + str(e) + "|" + str(voltage))
-                recTest(e, d1, d2+1, d3)
+                recursion(e, d1, d2 + 1, d3)
+                data.remove(e)
             elif (e - voltage == 3):
                 # diff = 3
                 diff3.append("3|" + str(e) + "|" + str(voltage))
-                print("3|" + str(e) + "|" + str(voltage))
-                recTest(e, d1, d2, d3+1)
+                recursion(e, d1, d2, d3 + 1)
+                data.remove(e)
     else:
         pass
 
-temp = helper.getData("10")
+temp = helper.getData("00")
 
 # to int
 data = [int(e) for e in temp]
+data.sort()
 
+# set max as defined, append to list
 max = max(data) + 3
+data.append(max)
 
+# lists to append each connected adapter 
 diff1 = []
 diff2 = []
 diff3 = []
 
-recTest(0, 0, 0, 0)
-
-diff1 = list(set(diff1))
-diff2 = list(set(diff2))
-diff3 = list(set(diff3))
+# start recursion
+recursion(0, 0, 0, 0)
 
 print("diff1: " + str(len(diff1)))
-print("diff2: " + str(len(diff2)))
 print("diff3: " + str(len(diff3)))
+print("solution: " + str(len(diff1) * len(diff3)))
