031d388ce435830f0d298fdd6f50b0212174eb51
alexander-wurl <alexander.wurl@gmx.de>
2020-12-30 19:04:45 +0100

    part 1 encapsulated

--- adventofcode-02.py
+++ adventofcode-02.py
@@ -2,20 +2,24 @@
 
 import helper
 
-f = helper.getData("02")
+def part1():
+	f = helper.getData("02")
 
-solution = 0
+	solution = 0
 
-for line in f:
-	w = line.split(" ")
-	mm = w[0].split("-")
-	min = mm[0]
-	max = mm[1]
-	c = w[1][0]
-	count = w[2].count(c)
+	for line in f:
+		w = line.split(" ")
+		mm = w[0].split("-")
+		min = mm[0]
+		max = mm[1]
+		c = w[1][0]
+		count = w[2].count(c)
 
-	if (count >= int(min)) and (count <= int(max)):
-		solution += 1
-		print("OK " + str(solution))
+		if (count >= int(min)) and (count <= int(max)):
+			solution += 1
+			#print("OK " + str(solution))
 
-print("FINAL SOLUTION:" + str(solution))
+	print(solution)
+
+# main
+part1()
