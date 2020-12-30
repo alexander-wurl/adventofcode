e0eadd67d01afc61b84be4066b48b5abbe87a24a
alexander-wurl <alexander.wurl@gmx.de>
2020-12-30 19:06:02 +0100

    part 1 encapsulated

--- adventofcode-03.py
+++ adventofcode-03.py
@@ -2,21 +2,25 @@
 
 import helper
 
-file = helper.getData("03")
+def part1():
+    file = helper.getData("03")
 
-start = 0
-trees = 0
+    start = 0
+    trees = 0
 
-for line in file:
-    if (start > 30):
-        start = start - 31
+    for line in file:
+        if (start > 30):
+            start = start - 31
 
-    if (line[start] == '#'):
-        trees += 1
-        print(line[0:start] + 'X' + line[start+1:31])
-    else:
-        print(line[0:start] + 'O' + line[start+1:31])
+        if (line[start] == '#'):
+            trees += 1
+            #print(line[0:start] + 'X' + line[start+1:31])
+        #else:
+            #print(line[0:start] + 'O' + line[start+1:31])
 
-    start = start + 3
+        start = start + 3
 
-print("TREES: " + str(trees))
+    print(trees)
+
+# main
+part1()
