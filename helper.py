d5746339fd891c60fb7ae7ea68a180689816c72c
alexander-wurl <alexander.wurl@gmx.de>
2020-12-30 17:10:39 +0100

    concatenation replaced by placeholder and join

--- helper.py
+++ helper.py
@@ -1,6 +1,14 @@
 import os
 
 def getData(number):
-    fileId = open(os.path.dirname(__file__) + "/input-" + str(number) + ".txt")
-    data = fileId.read().split("\n")[:-1] 
+
+    # define filename
+    workingDir = os.path.dirname(__file__)
+    fileName = "input-{}.txt".format(number)
+    filePath = os.path.join(workingDir, fileName)
+
+    # read and split data
+    fileId = open(filePath)
+    data = fileId.read().split("\n")[:-1]
+
     return data
