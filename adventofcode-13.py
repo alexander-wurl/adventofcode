3d87c0702d6e4bed315c16cbf2eb39d17c6a2323
alexander-wurl <alexander.wurl@gmx.de>
2021-01-02 21:00:19 +0100

    code optimization

--- adventofcode-13.py
+++ adventofcode-13.py
@@ -2,55 +2,59 @@
 
 import helper
 
+# split 'data' by comma, remove 'x' from within, cast each value to int 
 def getBusIds(data):
-    return data.replace(",x", "").split(",")
+    return [int(e) for e in data.replace(",x", "").split(",")]
 
+# print timetable body
 def printTimeTable(time, info):
     print(time + "\t", end = "")
     for e in info:
         print(e + "\t", end = "")
     print("")
 
+def part1(eta, ids):
+    # set index and maximal waiting time using highest bus id
+    i = eta
+    max_waiting_time = i + max(ids)
+
+    # initial values
+    waiting_time = 9999
+    bus_id = 0
+
+    while (i <= max_waiting_time):
+
+        bus_stops = []
+
+        for e in ids:
+            if ((i % e) == 0):
+                bus_stops.append("D")
+                t = i - eta
+                if (waiting_time > t):
+                    waiting_time = t
+                    bus_id = e
+            else:
+                bus_stops.append("-")
+    
+        # optional: print time table body
+        #printTimeTable(str(i), bus_stops)
+
+        i += 1
+
+    print("solution for part 1: {}".format(bus_id * waiting_time))
+
+# main
+
 # data
 data = helper.getData("13")
 
 # estimated time of (my) arrival at bus stop
-eta = data[0]
+eta = int(data[0])
 
 # relevant bus ids
 ids = getBusIds(data[1])
 
-# optional: print first line of bus schedules
+# optional: print time table header
 #printTimeTable("time", ids)
 
-# cast eta and set max waiting using highest bus id
-i = int(eta)
-max_wating_time = i + int(max(ids))
-
-# initial values for 
-waiting_time = 9999
-bus_id = 0
-
-while (i <= max_wating_time):
-
-    bus_stops = []
-
-    for e in ids:
-        if ((i % int(e)) == 0):
-            bus_stops.append("D")
-            t = i - int(eta)
-            if (waiting_time > t):
-                waiting_time = t
-                bus_id = int(e)
-        else:
-            bus_stops.append("-")
-
-    # optional: print time table
-    #printTimeTable(str(i), bus_stops)
-
-    i += 1
-
-print("solution: {}".format(bus_id * waiting_time))
-
-
-
+part1(eta, ids)
