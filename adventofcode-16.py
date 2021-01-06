df8360052feaf32baa268bd30d8981538edd0b80
alexander-wurl <alexander.wurl@gmx.de>
2021-01-06 20:09:03 +0100

    16 part 2 next try, still not working

--- adventofcode-16.py
+++ adventofcode-16.py
@@ -2,16 +2,31 @@
 
 import helper
 
-def checkValidity(value: int, note: list) -> bool:
-    valid = False
+def getValidTickets(tickets: list, notes: list):
 
-    if (((value >= note[0]) and (value <= note[1])) or ((value >= note[2]) and (value <= note[3]))):
+    validTickets = []
+
+    for ticket in tickets:
+
         valid = True
 
-    return valid
+        for value in ticket:
 
-def checkSimilarity(value: int, note: list) -> bool:
-    error = 0
+            for note in notes:
+
+                check = checkValidity(value, note)
+
+            if (not check):
+                valid = False
+                break
+        
+        if (valid):
+            validTickets.append(ticket)
+
+    return validTickets
+
+def checkValidity(value: int, note: list) -> bool:
+    valid = False
 
     if (((value >= note[0]) and (value <= note[1])) or ((value >= note[2]) and (value <= note[3]))):
         valid = True
@@ -59,14 +74,30 @@ def part1():
 
     print("Solution for part 1 is: {}.".format(ser))
 
+def checkNotesInColumn(notes:list, columnValues:list):
+
+    match = True
+    notesIndex = -1
+
+    for i in range(0, len(notes)):
+
+        for e in columnValues:
+            if (not checkValidity(e, notes[i])):
+                match = False
+
+        if match:
+            #print("Match: {}, {}".format(i, notes[i]))
+            notesIndex = i
+            break
+
+    return notesIndex
+
 def part2():
 
     data = helper.getData("16")
 
     notes = []
     tickets = []
-    
-    validTickets = []
 
     # notes = [
     #     [0, 1, 4, 19],
@@ -90,53 +121,26 @@ def part2():
        temp = e.split(",")
        tickets.append(list([int(value) for value in temp]))
 
-    validity = {}
-
-    # all tickets
-    for ticket in tickets:
-
-        # all values
-        for value in ticket:
-
-            # all notes
-            for note in notes:
-                validity.update({str(note): checkValidity(value, note)})
-
-        # check validity
-        if not False in validity.values():
-            validTickets.append(ticket)
-
-        validity.clear
-
-
-    # now process valid tickets
-    noteNumber = 0
-    columnNumber = 0
+    validTickets = getValidTickets(tickets, notes)
     
-    notesO = notes.copy()
-    noteLen = len(notesO)
+    # now process valid tickets
 
-    # columns
-    for columnNumber in range(0, len(validTickets[0])):
-        
-        for note in notes:
-
-            matchingNotes = True
-
-            for ticket in validTickets:
-
-                if (not checkValidity(ticket[columnNumber], note)):
-                    matchingNotes = False
-                    
-            
-            if (matchingNotes):
-                print("column number {} matches with note {}".format(columnNumber, note))
-        
+    # every column
 
 
+    for columnIndex in range(0, 19):
+
+        column = [c[columnIndex] for c in tickets]
+
+        index = checkNotesInColumn(notes, column)
+
+        if (index >= 0):
+            print("Match: {}".format)
+
+    
 
 # main
 
-part1()
+#part1()
 
-#part2() # still not working :(
+part2() # still not working :(
