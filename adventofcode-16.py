d98e8043f9565df2655829c31cc9c227b36d0e19
alexander-wurl <alexander.wurl@gmx.de>
2021-01-08 03:21:58 +0100

    solution found by elimination, algorithm to do

--- adventofcode-16.py
+++ adventofcode-16.py
@@ -5,31 +5,59 @@ import helper
 def getValidTickets(tickets: list, notes: list):
 
     validTickets = []
+    invalidvalue = -1
 
     for ticket in tickets:
 
         valid = True
+        invalidvalue = -1
 
         for value in ticket:
+            
+            if (valid == False):
+                break
+
+            val = {}
 
             for note in notes:
 
                 check = checkValidity(value, note)
 
-            if (not check):
+                val[str(note)] = check
+
+            #print(val)
+            
+            if (True in val.values()):
+                valid = True
+            else:
                 valid = False
+                invalidvalue = value
+                #print("Mismatch")
                 break
-        
+
         if (valid):
             validTickets.append(ticket)
+        #else:
+        #    print("Value {} in ticket {} not valid!".format(invalidvalue, ticket))
 
     return validTickets
 
+# check single value (for one note)
 def checkValidity(value: int, note: list) -> bool:
     valid = False
 
     if (((value >= note[0]) and (value <= note[1])) or ((value >= note[2]) and (value <= note[3]))):
         valid = True
+
+    return valid
+
+# check multiple values (for one note), one mismatch means column is not valid for given note
+def checkValidities(values: list, note: list) -> bool:
+    valid = True
+
+    for i in values:
+        if not (((i >= note[0]) and (i <= note[1])) or ((i >= note[2]) and (i <= note[3]))):
+            valid = False
 
     return valid
 
@@ -74,24 +102,6 @@ def part1():
 
     print("Solution for part 1 is: {}.".format(ser))
 
-def checkNotesInColumn(notes:list, columnValues:list):
-
-    match = True
-    notesIndex = -1
-
-    for i in range(0, len(notes)):
-
-        for e in columnValues:
-            if (not checkValidity(e, notes[i])):
-                match = False
-
-        if match:
-            #print("Match: {}, {}".format(i, notes[i]))
-            notesIndex = i
-            break
-
-    return notesIndex
-
 def part2():
 
     data = helper.getData("16")
@@ -117,30 +127,27 @@ def part2():
        notes.append(list([int(value) for value in temp]))
 
     #read tickets
-    for e in data[26:]:
+    for e in data[25:]:
        temp = e.split(",")
        tickets.append(list([int(value) for value in temp]))
 
     validTickets = getValidTickets(tickets, notes)
     
     # now process valid tickets
+    for note in notes:
 
-    # every column
+        for columnIndex in range(0, len(validTickets[0])):
 
+            column = [t[columnIndex] for t in validTickets]
 
-    for columnIndex in range(0, 19):
-
-        column = [c[columnIndex] for c in tickets]
-
-        index = checkNotesInColumn(notes, column)
-
-        if (index >= 0):
-            print("Match: {}".format)
-
-    
+            if (checkValidities(column, note)):
+                print("Note {}: matches with column {}".format(note, columnIndex))
 
 # main
 
-#part1()
+part1()
 
-part2() # still not working :(
+part2() # solution found by manual elimination, algorithm still to do
+
+solution = 157*113*53*101*167*191
+print("solution for part 2 is: {}".format(solution))
