0282a0a47f9bbf0c5960811fc0cc9155d5081420
alexander-wurl <alexander.wurl@gmx.de>
2021-02-02 20:50:49 +0100

    unit test's removed, clean up

--- Python/adventofcode-18.py
+++ Python/adventofcode-18.py
@@ -45,38 +45,10 @@ def part1() -> int:
     ret = 0
 
     for d in data:
-        value = EvalExpressionWithPrecedences(d)
-        ret += value
+        ret += EvalExpressionWithPrecedences(d)
 
     return ret
 
 # main
-
-#t1 = "1 + 2 * 3 + 4 * 5 + 6" # 71 OK
-#print( EvalExpressionWithPrecedences(t1) )
-
-#t2 = "1 + (2 * 3) + (4 * (5 + 6))" # 51 OK
-#print( EvalExpressionWithPrecedences(t2) )
-
-#t3 = "(2 * 3) + (4 * (5 + 6))" # 50 OK
-#print( EvalExpressionWithPrecedences(t3) )
-
-#t4 = "(2 * 3)" # 6 OK
-#print( EvalExpressionWithPrecedences(t4) )
-
-#t5 = "2 * 3 + (4 * 5)" #26 OK
-#print( EvalExpressionWithPrecedences(t5) )
-
-#t6 = "5 + (8 * 3 + 9 + 3 * 4 * 3)" #437 OK
-#print( EvalExpressionWithPrecedences(t6) )
-
-#t7 = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))" # 12240 OK
-#print( EvalExpressionWithPrecedences(t7) )
-
-#t8 = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" # 13632 OK
-#print( EvalExpressionWithPrecedences(t8) )
-
-#t9 = "8 * (9 * 7 * 5 + (9 + 8 * 4)) * 9 + (9 * 9 * (3 * 8 + 4) * 9)" #  47988 FALSCH
-#print( EvalExpressionWithPrecedences(t9) )
 
 print("solution for part 1: {}".format(part1()))
