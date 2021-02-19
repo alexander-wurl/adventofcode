3fc241ab75cdb670f7b20e44dc66f22e4c9b1e7e
Alexander Wurl <sun-tzu@gmx.net>
2021-02-19 21:39:49 +0100

    parameter for getData corrected

--- Python/adventofcode-07.py
+++ Python/adventofcode-07.py
@@ -3,29 +3,38 @@
 import helper
 
 def substitute(shinygoldrules, rules):
-  for r in rules:  
-    for g in shinygoldrules:  
-      if (r[1].find(g[0][:-1])>-1):  
+
+  for r in rules:
+
+    for g in shinygoldrules:
+
+      if (r[1].find(g[0][:-1])>-1):
         shinygoldrules.append([str(r[0]), str(r[1].replace(g[0][:-1], g[1]))])
+
   return shinygoldrules
 
 def makeunique(rules):
   leftside = [e[0] for e in rules]
   ret = []
+
   for e in leftside:
+
     if (e not in ret):
       ret.append(e)
+
   return ret
 
 
 def part1():
 
-  rules = helper.getData("07")
+  rules = helper.getData("7")
 
   splittedrules = [x.split(" contain ") for x in rules] 
 
   shinygoldrules = []
-  for e in splittedrules: 
+
+  for e in splittedrules:
+
     if (e[1].find("shiny gold bag")>-1): 
       shinygoldrules.append(e)
 
@@ -34,14 +43,15 @@ def part1():
   while True:
     shinygoldrules = substitute(shinygoldrules, splittedrules)
     l = len(makeunique(shinygoldrules))
+
     if ( l > solution):
         solution = l
     else:
         break
 
-  print( "solution: " + str(solution) )
+  return str(solution)
 
 # main
-part1()
+print("solution for part 1: {}".format(part1()))
 
 
