6f27ff1cc2e1a96b6dd5c34a49416a9b910079f8
Alexander Wurl <sun-tzu@gmx.net>
2021-02-19 22:35:22 +0100

    minor changes

--- Python/adventofcode-15.py
+++ Python/adventofcode-15.py
@@ -16,13 +16,11 @@ def isInDict(found, memory) -> int:
 def startGame(n: int):
 
     memory = {1: 14, 2: 3, 3: 1, 4: 0, 5: 9, 6: 5}
-
     currentTurn = list(memory)[-1] + 1
 
     while (currentTurn <= n):
 
         lastValue = memory.get(currentTurn - 1)
-
         index = isInDict(lastValue, memory)
 
         if (index > 0):
@@ -30,19 +28,16 @@ def startGame(n: int):
             del memory[index]
         else:
             numberSpoken = 0
-    
+
         memory[currentTurn] = numberSpoken
-        
         currentTurn += 1
-        print(currentTurn)
 
-    print("number spoken: {}".format(numberSpoken))
-
+    return numberSpoken
 
 # main
 
 # part 1
-#startGame(2020)
+print("solution for part 1: {}".format(startGame(2020)))
 
 # part 2
-startGame(30000000)
+#print("solution for part 1: {}".format(startGame(30000000)))
